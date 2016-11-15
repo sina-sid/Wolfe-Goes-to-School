8e3ccab5b6b9d7adc259be5bfff672ead8617c0d
sina-sid <sinas@andrew.cmu.edu>
2016-11-15 01:52:12 -0500

    Working on dialogue display

---
+++ Assets/Scripts/Question1Selector.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Question1Selector : MonoBehaviour {
+	public Dictionary<int, string> question1_list = new Dictionary<int, string>();
+	Text question1;
+	int round_number = 0; //this needs to be declared somewhere else as a global variable
+
+	// Use this for initialization
+	void Start () {
+		question1 = GetComponent<Text>();
+
+
+		question1_list.Add(0, "Where were you last night?");
+		question1_list.Add(1, "Who do you trust?");
+		question1_list.Add(2, "QUESTION NOT AVAILABLE");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		question1.text = question1_list[round_number];
+	
+	}
+}
---
+++ Assets/Scripts/dialogueScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class dialogueScript : MonoBehaviour {
+
+
+
+
+
+	Text doctor_answer;
+
+	// Use this for initialization
+	void Start () {
+		Dictionary<string, string> round1_question1_doctor_answers = new Dictionary<string, string>();
+		doctor_answer = GetComponent<Text>();
+
+		round1_question1_doctor_answers.Add("Snarky", "If you must know, I was performing an operation - which was probably more important than anything you were doing.");
+		round1_question1_doctor_answers.Add("Dramatic", "Only saving lives!! Not that it matters now that this wolf is on the prowl.");
+		round1_question1_doctor_answers.Add("Humorous", "Well, I was wheeling a patient into the operation room, but then he had a change of heart.");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
