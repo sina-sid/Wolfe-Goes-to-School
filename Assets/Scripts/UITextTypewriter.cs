e910630322eafc7bc8563093791879f797cb0217
sina-sid <sinas@andrew.cmu.edu>
2016-11-20 17:59:02 -0500

    Added mockup of attendance

---
+++ Assets/Scripts/UITextTypewriter.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+// attach to UI Text component (with the full text already there)
+
+public class UITextTypewriter : MonoBehaviour
+{
+
+	Text txt;
+	public string story;
+
+	void Awake()
+	{
+		txt = GetComponent<Text>();
+
+		story = txt.text;
+		txt.text = "";
+
+		// TODO: add optional delay when to start
+		StartCoroutine("PlayText");
+	}
+
+	public void startAnimation ()
+	{
+		StartCoroutine("PlayText");
+	}
+
+	IEnumerator PlayText()
+	{
+		foreach (char c in story)
+		{
+			txt.text += c;
+			yield return new WaitForSeconds(0.03f);
+		}
+	}
+
+	public void clearText ()
+	{
+		StopAllCoroutines();
+		txt.text = "";
+	}
+
+}
