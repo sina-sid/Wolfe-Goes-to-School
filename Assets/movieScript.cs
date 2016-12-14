337e0f9ba1d423fabe94701b0257c8f5df8157d7
sina-sid <sinas@andrew.cmu.edu>
2016-12-13 23:52:07 -0500

    Added opening cutscene

---
+++ Assets/movieScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class movieScript : MonoBehaviour {
+
+	public float changeAfter = 16;
+
+	// Use this for initialization
+	void Start () {
+		((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int curTime = (int)(changeAfter - Time.timeSinceLevelLoad);
+		//		Debug.Log (curTime.ToString ());
+		if (curTime <= 0) {
+			SceneManager.LoadScene ("classroomStoryScene");
+		}
+	}
+}
